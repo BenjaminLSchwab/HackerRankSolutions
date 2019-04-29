75fdf2f6287d158394760897b9400997ce6712e5
Ben <benjamin.schwab.nonspam@gmail.com>
2019-04-29 12:52:57 -0700

    Score keeping solution

---
+++ C#/ScoreKeeping.cs
@@ -0,0 +1,50 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int currentHighest = scores[0];
+        int currentLowest = scores[0];
+        int[] ans = new int[2];
+        foreach(int score in scores){
+            if(score > currentHighest){
+                currentHighest = score;
+                ans[0]++;
+            }
+            else if(score < currentLowest){
+                currentLowest = score;
+                ans[1]++;
+            }
+        }
+        return ans;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp))
+        ;
+        int[] result = breakingRecords(scores);
+
+        textWriter.WriteLine(string.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
